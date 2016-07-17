04c95024ef435d4289ce56fb0c8324a9231197af
Nisha <nishapmore@gmail.com>
2016-07-17 18:41:16 +0530

    added one assert statement.

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/FetchJokeTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/FetchJokeTest.java
@@ -2,7 +2,6 @@ package com.udacity.gradle.builditbigger;
 
 import android.test.AndroidTestCase;
 import android.test.mock.MockContext;
-import android.util.Log;
 
 import org.mockito.Mock;
 
@@ -20,19 +19,15 @@ public class FetchJokeTest extends AndroidTestCase{
     protected void setUp() throws Exception {
         super.setUp();
         joke = null;
-        fetchJoke = new FetchJoke(mockContext);
 
+        fetchJoke = new FetchJoke(mockContext);
+        assertNotNull(fetchJoke);
     }
     public void testFetchJoke(){
         try {
             if(fetchJoke!=null){
                 joke = fetchJoke.execute().get(30, TimeUnit.SECONDS);
             }
-            else {
-                Log.d("MyLOG","null asynctask");
-            }
-
-
             assertNotNull(joke);
         } catch (Exception e) {
             fail("Timeout");
