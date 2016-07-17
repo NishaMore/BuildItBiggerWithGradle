91e1f88c691ba5640b6b5de68424fb5916f8b6bf
Nisha <nishapmore@gmail.com>
2016-07-17 18:40:32 +0530

    added loading spinner functionality

--- app/src/free/java/com/udacity/gradle/builditbigger/MainFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
@@ -19,6 +20,9 @@ public class MainFragment extends Fragment {
 
     private AdView mAdView;
     private InterstitialAd mInterstitialAd;
+    private ProgressBar mProgressBar;
+    private String joke;
+    private static final String JOKE_EXTRA = "joke_extra";
 
     public MainFragment(){}
 
@@ -26,11 +30,24 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mProgressBar = (ProgressBar) root.findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
         mAdView = (AdView) root.findViewById(R.id.adView);
         showBannerAd();
+
         mInterstitialAd = new InterstitialAd(getActivity());
         mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
         requestNewInterstitial();
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                mProgressBar.setVisibility(View.VISIBLE);
+                new FetchJoke(getContext()).execute();
+
+            }
+        });
         Button button = (Button) root.findViewById(R.id.tellJokeBtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,17 +65,12 @@ public class MainFragment extends Fragment {
         if (mInterstitialAd.isLoaded()) {
             mInterstitialAd.show();
         } else {
-            requestNewInterstitial();
+            //requestNewInterstitial();
+            mProgressBar.setVisibility(View.VISIBLE);
+            new FetchJoke(getContext()).execute();
         }
 
-        mInterstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                requestNewInterstitial();
-                new FetchJoke(getContext()).execute();
 
-            }
-        });
     }
 
     private void requestNewInterstitial() {
@@ -78,4 +90,9 @@ public class MainFragment extends Fragment {
 
     }
 
+    @Override
+    public void onPause() {
+        mProgressBar.setVisibility(View.GONE);
+        super.onPause();
+    }
 }
