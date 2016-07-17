96d51440d515727a92dad8a95122b836f96605ad
Nisha <nishapmore@gmail.com>
2016-07-17 18:42:44 +0530

    added loading functionality.

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainFragment.java
@@ -9,12 +9,13 @@ import android.view.ViewGroup;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
+import android.widget.ProgressBar;
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainFragment extends Fragment {
 
+    private ProgressBar mProgressBar;
 
     public MainFragment() {
         // Required empty public constructor
@@ -26,15 +27,24 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
         Button button = (Button) root.findViewById(R.id.tellJokeBtn);
+        mProgressBar = (ProgressBar) root.findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mProgressBar.setVisibility(View.VISIBLE);
                 new FetchJoke(getContext()).execute();
             }
         });
 
         return root;
 
+    }
+    @Override
+    public void onPause() {
+        mProgressBar.setVisibility(View.GONE);
+        super.onPause();
     }
 
 }
